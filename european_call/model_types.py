14ae20fef18a7f12b82d039602bb1964f25135a0
ASHISH DHIMAN <ashish1610dhiman@gmail.com>
2023-11-29 19:05:41 -0500

    made changes b

--- european_call/model_types.py
+++ european_call/model_types.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch
 import torch.nn.init as init
 
-class EuropeanCall_gated(nn.Module):
+class EuropeanCall_gated1(nn.Module):
     def __init__(self, N_INPUT, N_OUTPUT, N_HIDDEN, N_LAYERS):
-        super(EuropeanCall_gated, self).__init__()
+        super(EuropeanCall_gated1, self).__init__()
         self.N_HIDDEN = N_HIDDEN
         self.activation1 = nn.LeakyReLU(negative_slope=0.2)
         self.activation2 = nn.Tanh()
@@ -43,3 +43,48 @@ class EuropeanCall_gated(nn.Module):
         wh = self.w2_layer(h_x)
         y_net = wx*yx + wh*yh
         return y_net
+    
+
+class EuropeanCall_gated2(nn.Module):
+    def __init__(self, N_INPUT, N_OUTPUT, N_HIDDEN, N_LAYERS):
+        super(EuropeanCall_gated2 self).__init__()
+        self.N_HIDDEN = N_HIDDEN
+        self.activation1 = nn.LeakyReLU(negative_slope=0.2)
+        self.activation2 = nn.Tanh()
+        self.fcs1 = nn.Sequential(
+            nn.Linear(N_INPUT, N_HIDDEN),
+            self.activation2)
+        self.fcs2 = nn.Sequential(
+            nn.Linear(N_INPUT, N_HIDDEN),
+            self.activation2)
+        self.fch = nn.Sequential(*[
+            nn.Sequential(
+                nn.BatchNorm1d(N_HIDDEN),
+                nn.Linear(N_HIDDEN, N_HIDDEN),
+                self.activation2
+            ) for _ in range(N_LAYERS)])
+        self.fce1 = nn.Sequential(*[nn.Linear(N_HIDDEN, N_OUTPUT)])
+        self.fce2 = nn.Sequential(*[nn.Linear(N_HIDDEN, N_OUTPUT)])
+        self.w1_layer = nn.Sequential(*[nn.Linear(N_HIDDEN + N_HIDDEN, N_OUTPUT),self.activation2])
+        self.w2_layer = nn.Sequential(*[nn.Linear(N_HIDDEN + N_HIDDEN, N_OUTPUT),self.activation2])
+        # init.xavier_uniform_(self.fcs1.weight)
+        # init.xavier_uniform_(self.fcs2.weight)
+        # init.xavier_uniform_(self.fch.weight)
+
+    def forward(self, x):
+        # Apply the first layer
+        I1 = self.fcs1(x)
+        H1 = I1
+        # Apply hidden layers with residual connections
+        for layer in self.fch:
+            H1 = layer(H1) + H1
+        # Apply the final layer
+        H2 = self.fcs2(x) #1
+        y1 =  self.fce1(H1)#1D
+        y2 =  self.fce2(H2)#1D
+        H_cat = torch.cat([H1,H2],axis=1)
+        # print (h_x.shape)
+        w1 = self.w1_layer(H_cat)
+        w2 = self.w2_layer(H_cat)
+        y_net = w1*y1 + w2*y2
+        return y_net    
